4d248df0d001e1f12800651cf28355d4249748c8
Arie M. Prasetyo <ari.setyo@gmail.com>
2021-06-20 21:17:18 +0700

    some minor fixes and update on package json app names

--- 006-ssr-react-app/src/App.js
+++ 006-ssr-react-app/src/App.js
@@ -9,9 +9,6 @@ import styles from './App.css';
 
 /**
  * The main React application component
- * 
- * Note:
- * In this example, the App component is a stateless React component.
  */
 const App = () => (
 	<div
@@ -20,7 +17,7 @@ const App = () => (
 		<h1>Hello world!</h1>
 
 		<p>
-			This is <code>amp-react-app</code> 👋🏽
+			I am generated from the server 👋🏽
 		</p>
 
 	</div>	
